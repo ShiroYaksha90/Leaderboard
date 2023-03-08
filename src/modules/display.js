f3df5b28f6b24ce82dd684e316a1682fe9cb54e0
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-03-08 14:41:06 +0300

    Add index.html file Add index.js file Add style.css file Add display.js file

---
+++ src/modules/display.js
@@ -0,0 +1,7 @@
+const data = [{name:'Name', score: 100},
+{name:'Name', score: 20},
+{name:'Name', score: 50},
+{name:'Name', score: 78},
+{name:'Name', score: 125},
+{name:'Name', score: 77},
+{name:'Name', score: 24},]
