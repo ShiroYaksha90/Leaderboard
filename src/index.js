57fc682c92d233a9887aa1f32106ed33fd1639f2
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-03-09 18:45:16 +0300

    Fix eslint issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './style.css';
-import ApiOp from './modules/display.js'
-const refereshButton = document.getElementById('referesh')
-const form = document.querySelector('#form')
+import ApiOp from './modules/display.js';
+
+const refereshButton = document.getElementById('referesh');
+const form = document.querySelector('#form');
 
 document.addEventListener('DOMContentLoaded', () => {
   ApiOp.render();
@@ -11,7 +12,7 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   ApiOp.newScore();
   ApiOp.clearFormInput();
-})
+});
 refereshButton.addEventListener('click', () => {
   ApiOp.render();
-})
+});
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,48 +1,49 @@
-const submit = document.getElementById('submit');
 const userName = document.getElementById('user-name');
 const userScore = document.getElementById('user-score');
 
 const player = {
   user: String,
-  score: Number
-}
+  score: Number,
+};
 
 const gameName = {
-  name: 'Random scores'
-}
+  name: 'Random scores',
+};
 
 export default class ApiOp {
   static newGame = async () => {
     const reply = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
-    {
-      method: 'POST',
-      body: JSON.stringify(gameName),
-      headers: {
-        'content-type': 'application/json'
-      }
-    });
+      {
+        method: 'POST',
+        body: JSON.stringify(gameName),
+        headers: {
+          'content-type': 'application/json',
+        },
+      });
     const data = await reply.json();
     return data;
   }
+
   static newScore = async () => {
-    if (userName.value.length !== 0 && userScore.value.length !==0){
+    if (userName.value.length !== 0 && userScore.value.length !== 0) {
       player.user = userName.value;
       player.score = userScore.value;
     }
     const reply = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ABCDEFGHIJklmnopqrst/scores/', {
       method: 'POST',
       headers: {
-        'content-type': 'application/json'
+        'content-type': 'application/json',
       },
-      body: JSON.stringify(player)
+      body: JSON.stringify(player),
     });
     const data = reply.json();
     return data;
   }
+
   static fetchScores = async () => {
     const reply = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ABCDEFGHIJklmnopqrst/scores/', {
       headers: {
-        'content-type': 'application/json'
+        'content-type': 'application/json',
       },
     });
     const data = await reply.json();
@@ -50,21 +51,22 @@ export default class ApiOp {
   }
 
   static render = () => {
-      const board = document.querySelector('.scores');
-      ApiOp.fetchScores().then((data) => {
-        let itemElements = '';
-        const scores = data.result;
-        scores.map((score) => {
-          itemElements += `
+    const board = document.querySelector('.scores');
+    ApiOp.fetchScores().then((data) => {
+      let itemElements = '';
+      const scores = data.result;
+      scores.map((score) => {
+        itemElements += `
           <li class="item"> ${score.user}: ${score.score}`;
-          return itemElements
-        })
-        board.innerHTML = itemElements;
-        return itemElements
-      }) 
-    }
-    static clearFormInput(){
-      userName.value="";
-      userScore.value=''
-    }
+        return itemElements;
+      });
+      board.innerHTML = itemElements;
+      return itemElements;
+    });
+  }
+
+  static clearFormInput() {
+    userName.value = '';
+    userScore.value = '';
+  }
 }
